6f151bdf2e4162c6fc7d45c8eacf4726838afd2e
Jahanggir Jaman <jahanggir-jaman@users.noreply.github.com>
2019-03-09 23:29:04 +0600

    added responsive carousel

--- src/components/MainCarousel/MainCarousel.js
+++ src/components/MainCarousel/MainCarousel.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 export default class MainCarousel extends Component {
   constructor() {
@@ -7,7 +9,28 @@ export default class MainCarousel extends Component {
   render() {
     return (
       <>
-        <div className="topbar">Topbar</div>
+        <Carousel
+          // showThumbs={this.props.showThumb}
+          showThumbs={false}
+          showStatus={false}
+          showIndicators={true}
+          useKeyboardArrows
+          infiniteLoop
+          emulateTouch
+        >
+          <div>
+            <img src="https://via.placeholder.com/1280x720?text=Hello+From+Jaman" />
+            <p className="legend">Legend 1</p>
+          </div>
+          <div>
+            <img src="https://via.placeholder.com/1280x720?text=Hello" />
+            <p className="legend">Legend 2</p>
+          </div>
+          <div>
+            <img src="https://via.placeholder.com/1280x720?text=From+Jaman" />
+            <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
       </>
     );
   }
